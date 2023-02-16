c26b4760c236d7413c690fa698978bc052794a85
Shenghao <shenghaodong18@gmail.com>
2023-02-16 13:34:22 -0500

    added more comments

--- funcs.cpp
+++ funcs.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
+/*
+Knight can move in a L shape:
+Ex:
+K**
+  *
+  *
+3 --> 2
+*/
 std::string solveKnight(){
     //Recursively Solve
     //Base Case
-
+    
 
     //If Base Case not met solve for knight positions otherwise return
 
