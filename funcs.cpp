86971a119211d8a6eb79657fc2b2e79cf808ae23
Shenghao <shenghaodong18@gmail.com>
2023-02-21 09:56:29 -0500

    added a bit more comments on recursion

--- funcs.cpp
+++ funcs.cpp
@@ -7,11 +7,18 @@ K**
   *
 3 --> 2
 */
-std::string solveKnight(){
+std::string solveKnight(std::string[][] board){
     //Recursively Solve
     //Base Case
-    
+    if(){
+
+    }else{
+      //Solve Up (left) then Up (right)
+      //Solve Down (left) then Down (right)
+    }
 
     //If Base Case not met solve for knight positions otherwise return
 
 }
+//Mark spot on Arraylist as "S" if solved so the program knows that the spot is already solved 
+//add a "return" if the program doesn't find any solutions
