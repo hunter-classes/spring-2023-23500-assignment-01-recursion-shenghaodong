8f94cf32e657f5377f0ac7e1579ed411cc8faeb3
Shenghao Dong <shenghaodong18@gmail.com>
2023-02-26 00:23:18 -0500

    added some stuff to recursion

--- funcs.cpp
+++ funcs.cpp
@@ -7,10 +7,34 @@ K**
   *
 3 --> 2
 */
-std::string solveKnight(){
+std::string solveKnight(char[][] chessboard, int rowStart, int colStart){
     //Recursively Solve
     //Base Case
-    
+    int counter = 0;
+    if(counter == 25){
+      return "";
+    }else if(counter ){
+
+
+    }else{
+      //recurse 8 times for each way the knight can move
+
+      //Up
+      solveKnight(chessboard, row, col);
+      solveKnight(chessboard, row, col);
+
+      //Down
+      solveKnight(chessboard, row, col);
+      solveKnight(chessboard, row, col);
+
+      //Left
+      solveKnight(chessboard, row, col);
+      solveKnight(chessboard, row, col);
+
+      //Right
+      solveKnight(chessboard, row, col);
+      solveKnight(chessboard, row, col);
+    }
 
     //If Base Case not met solve for knight positions otherwise return
 
